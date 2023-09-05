598a6e96c34d56342c34ca7bcf777c7e27faad38
K4kug3n <kakugen@yahoo.fr>
2023-09-05 23:30:06 +0200

    Basic viewport

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 pub mod vec3;
 pub mod color;
+pub mod ray;
 
-use color::Color;
+use crate::color::Color;
+use crate::vec3::{Vec3, Point3};
+use crate::ray::Ray;
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction().unit();
+    let a = 0.5 * (unit_direction.y() + 1.);
+
+    (1. - a) * Color::new(1., 1., 1.) + a * Color::new(0.5, 0.7, 1.)
+}
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+
+    let aspect_ratio = 16. / 9.;
+    let image_width = 400;
+    let image_height = f64::max(f64::from(image_width) / aspect_ratio, 1.) as i32;
+
+    let focal_length = 1.;
+    let viewport_height = 2.;
+    let viewport_width = viewport_height * (f64::from(image_width) / f64::from(image_height));
+    let camera_center = Point3::new(0., 0., 0.);
+
+    let viewport_u = Vec3::new(viewport_width, 0., 0.);
+    let viewport_v = Vec3::new(0., -viewport_height, 0.);
+
+    let pixel_delta_u = viewport_u / f64::from(image_width);
+    let pixel_delta_v = viewport_v / f64::from(image_height);
+
+    let viewport_upper_left = camera_center - viewport_u / 2. - viewport_v / 2. - Vec3::new(0., 0., focal_length);
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     println!("P3");
     println!("{} {}", image_width, image_height);
@@ -14,9 +40,13 @@ fn main() {
     for j in 0..image_height {
         eprint!("\rScanlines remaining: {} ", image_height - j);
         for i in 0..image_width {
-            
-            let color = Color::new(f64::from(i) / f64::from(image_width - 1), f64::from(j) / f64::from(image_height - 1), 0.);
-            color.write();
+            let pixel_center = pixel00_loc + (pixel_delta_v * f64::from(j)) + (pixel_delta_u * f64::from(i));
+            let ray_direction = pixel_center - camera_center;
+
+            let ray = Ray::new(camera_center, ray_direction);
+
+            let pixel_color = ray_color(&ray);
+            pixel_color.write();
         }
     }
 
