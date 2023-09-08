89d758673ab8321135da8ea2110a472f3723f302
K4kug3n <maxime.rambosson@gmail.com>
2023-09-09 01:08:24 +0200

    Impl schlick approximation

--- src/dielectric.rs
+++ src/dielectric.rs
@@ -3,6 +3,8 @@ use crate::ray::Ray;
 use crate::hit_record::HitRecord;
 use crate::color::Color;
 
+use rand::Rng;
+
 pub struct Dielectric {
 	refraction_coeff: f64,
 }
@@ -15,12 +17,12 @@ impl Material for Dielectric {
 		let cos_theta = f64::min((-unit_direction).dot(rec.normal), 1.0);
 		let sin_theta = f64::sqrt(1. - cos_theta * cos_theta);
 
-		if (refraction_ratio * sin_theta) > 1.0 {
+		let cannot_refract = (refraction_ratio * sin_theta) > 1.0;
+		if cannot_refract || Dielectric::reflectance(cos_theta, self.refraction_coeff) > rand::thread_rng().gen() {
 			let reflected = unit_direction.reflect(&rec.normal);
 
 			return Some((Ray::new(rec.point, reflected), Color::new(1., 1., 1.)));
 		}
-		
 		
 		let refracted = unit_direction.refract(&rec.normal, refraction_ratio);
 
@@ -33,5 +35,12 @@ impl Dielectric {
 		Dielectric { 
 			refraction_coeff
 		}
+	}
+
+	fn reflectance(cosine: f64, refraction_coeff: f64) -> f64 {
+		let mut r0 = (1. - refraction_coeff) / (1. + refraction_coeff);
+		r0 = r0 * r0;
+
+		r0 + (1. - r0) * f64::powf(1. - cosine, 5.)
 	}
 }
