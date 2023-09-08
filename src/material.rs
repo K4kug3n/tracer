a24b0a9bf11418adcac88ce4a42b50d5c94f0a3d
K4kug3n <maxime.rambosson@gmail.com>
2023-09-08 21:33:22 +0200

    Impl lambertian

--- src/material.rs
+++ src/material.rs
@@ -3,5 +3,5 @@ use crate::hit_record::HitRecord;
 use crate::color::Color;
 
 pub trait Material {
-	fn scatter(&self, ray: &Ray, rec: &HitRecord, attenuation: &Color) -> Option<Ray>;
+	fn scatter(&self, ray: &Ray, rec: &HitRecord) -> (Ray, Color);
 }
